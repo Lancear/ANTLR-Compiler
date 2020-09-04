51c6e5bcd07ccc7ac80e7bc8c2581253abfb03a3
Lance J <lancear153@gmail.com>
2020-09-04 11:49:06 +0200

    removed project since its a package in the other project now

--- jvm-class-generator/test.sh
+++
@@ -1,8 +0,0 @@
-src="./src"
-bin="./bin"
-
-testRunner="jvm_class_generator.tests.TestRunner"
-testRunnerPath="$src/jvm_class_generator/tests/TestRunner.java"
-
-javac -sourcepath $src -d $bin $testRunnerPath
-java -classpath $bin $testRunner
